f3cd9b259e46d171ecc3a295006affcd18042a93
gadela <gadela.confluence@gmail.com>
2023-10-08 13:36:38 +1100

    Active Link in the NavBAR

--- issue-tracker/src/app/NavBar.tsx
+++ issue-tracker/src/app/NavBar.tsx
@@ -1,10 +1,17 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
 import {AiFillBug} from 'react-icons/ai'
+import { usePathname } from 'next/navigation'
+import classnames from 'classnames'
 
 
 const NavBar = () => {
-    const links = [
+  const currentPath = usePathname()
+
+  console.log(currentPath)
+
+  const links = [
         {label: 'Dashboard', href:'/'},
         {label: 'Issues', href:'/issues'}
     ]
@@ -15,7 +22,12 @@ const NavBar = () => {
             {links.map(link => 
             <Link 
             key={link.href} 
-            className='text-zinc-500 hover:text-zinc-800 transition-colors' 
+            // className= { `${link.href === currentPath ? 'text-zinc-900' : 'text-zinc-500'} hover:text-zinc-800 transition-colors` }
+            className ={classnames({
+              'text-zinc-900' : link.href === currentPath,
+              'text-zinc-500': link.href !== currentPath,
+              'hover:text-zinc-800 transition-colors' : true
+            })}
             href={link.href}> 
             {link.label}
             </Link>
--- issue-tracker/src/app/page.tsx
+++ issue-tracker/src/app/page.tsx
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-    <div> Hello NextJS </div>
+    <div> Home Page </div>
   )
 }
