9d7c4670a519e890925c64ed8936628c90ba1c60
gadela <gadela.confluence@gmail.com>
2023-10-08 13:43:10 +1100

    Bit of Clean up

--- issue-tracker/src/app/NavBar.tsx
+++ issue-tracker/src/app/NavBar.tsx
@@ -9,8 +9,6 @@ import classnames from 'classnames'
 const NavBar = () => {
   const currentPath = usePathname()
 
-  console.log(currentPath)
-
   const links = [
         {label: 'Dashboard', href:'/'},
         {label: 'Issues', href:'/issues'}
